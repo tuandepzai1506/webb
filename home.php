21289ae37505d0ef52a960f37a221c24ee437bdc
Duyen <imanxy09@gmail.com>
2025-06-07 23:26:53 +0700

    add home.php & css

---
+++ home.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <title>TECHSTORE - Trang chủ</title>
+    <link rel="stylesheet" type="text/css" href="./home.css">
+</head>
+<body>
+
+    <!--HEADER-->
+    <header>
+        
+        <div class="left-header">
+            <a href="#">
+                <img class="logo" src="./image/ChatGPT Image Jun 7, 2025, 07_38_44 PM.png" alt="" width="50" height="50">
+            </a>
+            <h2 class="slogan">&nbsp;Cool Tech, Real Vibes</h2>
+        </div>
+
+        <div class="right-header">
+            <form class="search-form" action="#" method="get">
+                <input type="text" name="q" placeholder="Tìm kiếm...">
+                <button type="submit">🔍</button>
+            </form>
+
+            <div class="separator"></div>
+
+            <a href="#" class="login-btn">Đăng nhập</a>
+        </div>
+
+    </header>
+
+    <!--MENU-->
+    <nav>
+        <ul>
+            <li><a href="#">Trang chủ</a></li>
+            <li><a href="#">Sản phẩm</a>
+                <ul>
+                    <li><a href="#">Laptop</a></li>
+                    <li><a href="#">Camera</a></li>
+                    <li><a href="#">Phụ kiện</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Tin tức</a></li>
+            <li><a href="#">Liên hệ</a></li>
+            <li><a href="#">Hỗ trợ</a></li>
+        </ul>
+    </nav>
+
+    <!--MAIN-->
+    <section>
+
+        <!--LEFT-->
+        <aside id="aside-left">
+            <p>L</p>
+        </aside>
+
+        <!--CONTENT-->
+        <article>
+            <p>C</p>
+        </article>
+
+        <!--RIGHT-->
+        <aside id="aside-right">
+            <p>R</p>
+        </aside>
+
+    </section>
+
+    <!--FOOTER-->
+    <footer>
+        <div class="copyright">
+            <p>&copy; 2025 TECHSTORE</p>
+        </div>
+    </footer>
+
+</body>
+</html>
