0376631ca314b0984eb0756047889f7e18870102
tuandepzai1506 <ntt150624@gmail.com>
2025-07-05 00:24:31 +0700

    webb

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <title>TECHSTORE - Trang chủ</title>
+    <title>HEARTTECH - Trang chủ</title>
     <link rel="stylesheet" type="text/css" href="./main.css">
     <link rel="stylesheet" href="./themify-icons/themify-icons.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
